f76e66b898cc12bb5c0adb74f76ef470d6b31199
David Tondre <44331704+dat1txn@users.noreply.github.com>
2018-10-21 21:51:56 -0700

    Update Assignment2.cpp

--- Assignment2/Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2/Assignment2.cpp
@@ -1,4 +1,4 @@
-﻿// Assignment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Assignment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -11,8 +11,8 @@ int main()
 	string filename, filename1, filename2, iline, oline, str;
 	string insize, outsize;
 	size_t found, found1, found2, found3, found4, found5, found6;
-	size_t found7, found8, found9, found10, found11;
-	int temp, bitsize = 0;
+	size_t found7, found8, found9, found10, found11, foundname1, foundname2;
+	int bittemp, temp, compare = 0, bitsize = 0;
 
 	cout << "Please enter filename: ";
 	cin >> filename;
@@ -36,37 +36,36 @@ int main()
 
 	while (getline(myfile1, iline))
 	{
-		found1 = iline.find("input");
-		if (found1 != string::npos)
+		foundname1 = iline.find("input");
+		if (foundname1 != string::npos)
 		{
-			iline = iline.substr(found1 + 6);
-			found1 = iline.find("Int");
-			iline = iline.substr(found1 + 3);
-			found2 = iline.find(" ");
-			if (found2 != string::npos)
+			iline = iline.substr(foundname1 + 6);
+			foundname1 = iline.find("Int");
+			iline = iline.substr(foundname1 + 3);
+			foundname2 = iline.find(" ");
+			if (foundname2 != string::npos)
 			{
-				insize = iline.substr(0, found2);
+				insize = iline.substr(0, foundname2);
 			}
 			
 		}
-		found1 = iline.find("output");
-		if (found1 != string::npos)
+		foundname1 = iline.find("output");
+		if (foundname1 != string::npos)
 		{
-			iline = iline.substr(found1 + 6);
-			found1 = iline.find("Int");
-			iline = iline.substr(found1 + 3);
-			found2 = iline.find(" ");
-			if (found2 != string::npos)
+			iline = iline.substr(foundname1 + 6);
+			foundname1 = iline.find("Int");
+			iline = iline.substr(foundname1 + 3);
+			foundname2 = iline.find(" ");
+			if (foundname2 != string::npos)
 			{
-				outsize = iline.substr(0, found2);
-				temp = stoi(outsize);
-				if (temp < bitsize)
+				outsize = iline.substr(0, foundname2);
+				bittemp = stoi(outsize);
+				if (bittemp < bitsize)
 					bitsize = bitsize;
 				else
-					bitsize = temp;
+					bitsize = bittemp;
 			}
 			oline = "module " + filename + "#(parameter DATAWIDTH = " + outsize + ")(a, b, c, z, x); \n";
-			//myfile2 << "input [DATAWIDTH/2-1:0] a, b, c;\n";
 		}
 
 		found = iline.find("=");
@@ -76,78 +75,84 @@ int main()
 			found1 = iline.find("+");
 			if (found1 != string::npos)
 			{
-				oline = oline + "ADD #(.DATAWIDTH(XX)) ADD_1(.a(a), .b(b), .sum(d)); \n";
-				temp = temp + 1;
+				temp = 1;
 				found1 = iline.find("+ 1");
 				if (found1 != string::npos)
 				{
 					oline = oline + "INC #(.DATAWIDTH(XX)) INC_1(.a(a), .b(b), .sum(d)); \n";
 				}
+				else
+				{
+					oline = oline + "ADD #(.DATAWIDTH(XX)) ADD_1(.a(a), .b(b), .sum(d)); \n";
+				}
 			}
 			found2 = iline.find("-");
 			if (found2 != string::npos)
 			{
-				oline = oline + "SUB #(.DATAWIDTH(XX)) SUB_1(.a(f), .b({8'b00000000, d}), .diff(xwire)); \n";
-				temp = temp + 1;
+				temp = 2;
 				found2 = iline.find("- 1");
 				if (found2 != string::npos)
 				{
 					oline = oline + "DEC #(.DATAWIDTH(XX)) DEC_1(.a(f), .b({8'b00000000, d}), .diff(xwire)); \n";
+				}
+				else
+				{
+					oline = oline + "SUB #(.DATAWIDTH(XX)) SUB_1(.a(f), .b({8'b00000000, d}), .diff(xwire)); \n";
 				}
 			}
 			found3 = iline.find("*");
 			if (found3 != string::npos)
 			{
 				oline = oline + "MUL #(.DATAWIDTH(XX)) MUL_1(.a(a), .b(c), .prod(f)); \n";
-				temp = temp + 1;
+				temp = 3;
 			}
 			found4 = iline.find("/");
 			if (found4 != string::npos)
 			{
 				oline = oline + "DIV #(.DATAWIDTH(XX)) DIV_1(.a(a), .b(b), .quot(e)); \n";
-				temp = temp + 1;
+				temp = 4;
 			}
 			found5 = iline.find("%");
 			if (found5 != string::npos)
 			{
 				oline = oline + "MOD #(.DATAWIDTH(XX)) MOD_1(.a(a), .b(c), .rem(g)); \n";
-				temp = temp + 1;
+				temp = 5;
 			}
 			found6 = iline.find("<<");
 			if (found6 != string::npos)
 			{
 				oline = oline + "SHL #(.DATAWIDTH(XX)) SHL_1(.a(g), .sh_amt({31'b0,dLTe}), .d(xwire)); \n";
-				temp = temp + 1;
+				temp = 6;
 			}
 			found7 = iline.find(">>");
 			if (found7 != string::npos)
 			{
 				oline = oline + "SHR #(.DATAWIDTH(XX)) SHR_1(.a(l2), .sh_amt({8'b00000000, sa}), .d(l2div2)); \n";
-				temp = temp + 1;
+				temp = 7;
 			}
 			found8 = iline.find("?");
 			if (found8 != string::npos)
 			{
 				oline = oline + "MUX2x1 #(.DATAWIDTH(XX)) MUX2x1(.a(d), .b(e), .sel(g), .d(z)); \n";
-				temp = temp + 1;
+				temp = 8;
 			}
 			found9 = iline.find("==");
 			if (found9 != string::npos)
 			{
-				oline = oline + "COMP #(.DATAWIDTH(XX) COMP_1(.a(d), .b(e), .gt(g)); \n";
-				temp = temp + 1;
+				oline = oline + "COMP #(.DATAWIDTH(XX)) COMP_1(.a(d), .b(e), .gt(g), .lt(l), .eq(e)); \n";
+				temp = 9;
 			}
 			found10 = iline.find("<");
-			if (found10 != string::npos)
+			if ((found10 != string::npos) && (temp != 6))
 			{
-				oline = oline + "COMP #(.DATAWIDTH(XX)) COMP_1(.a(d), .b(e), .gt(g)); \n";
-				temp = temp + 1;
+				oline = oline + "COMP #(.DATAWIDTH(XX)) COMP_1(.a(d), .b(e), .gt(g), .lt(l), .eq(e)); \n";
+				temp = 10;
 			}
 			found11 = iline.find(">");
-			if (found11 != string::npos)
+			if ((found11 != string::npos) && (temp != 7))
 			{
-				oline = oline + "COMP #(.DATAWIDTH(XX)) COMP_1(.a(d), .b(e), .gt(g)); \n";
-				temp = temp + 1;
+				oline = oline + "COMP #(.DATAWIDTH(XX)) COMP_1(.a(d), .b(e), .gt(g), .lt(l), .eq(e)); \n";
+				temp = 11;
 			}
 			if ((found != string::npos) && (temp == 0))
 			{
